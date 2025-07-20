a8b17a4b44b99140cbcc994bc1d9a3ff69fd2319
Saarthi <saarthishiksha889@gmail.com>
2025-07-20 09:38:51 +0530

    Add files via upload

---
+++ day17.py
@@ -0,0 +1,12 @@
+#without use start and step
+# for i in range(5):
+#     print(i)
+# # with use start and end
+# for i in range(2, 8):
+#     print(i)
+# # with use start, end and step
+# for i in range(1, 10, 2):   
+#     print(i)
+# # with use start, end and negative step
+for i in range(10, 0, -2):                          
+    print(i)
